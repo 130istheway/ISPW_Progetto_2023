e8bc2092bb13ce715d8e9c7d7263206bfcf8bf19
Stefy <l.brigido45@gmail.com>
2023-10-28 21:43:28 +0200

    Updated articoliAlimentari in modo da essere una classe astratta che estende articoli.java

--- src/Articoli/ArticoliAlimentari/articoliAlimentari.java
+++ src/Articoli/ArticoliAlimentari/articoliAlimentari.java
@@ -1,5 +1,27 @@
 package Articoli.ArticoliAlimentari;
 
-public class articoliAlimentari {
+import Articoli.articoli;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class articoliAlimentari extends articoli {
+    private List<String> ingredienti = new ArrayList<>();
+    private double peso;
+
+    public void setIngredienti(List<String> ingredienti) {
+    }
+
+    public void setPeso(double peso) {
+    }
+
+    public List<String> getIngredienti() {
+        return ingredienti;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
     
 }
