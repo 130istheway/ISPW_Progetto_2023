6162e6a5582bb0e8d1464945eb2466c46388109c
Stefy <l.brigido45@gmail.com>
2023-10-28 22:03:15 +0200

    Update pane.java cosi da estendere la classe articoliAlimentari

--- src/Articoli/ArticoliAlimentari/pane.java
+++ src/Articoli/ArticoliAlimentari/pane.java
@@ -1,5 +1,57 @@
 package Articoli.ArticoliAlimentari;
 
-public class pane {
-    
+import Articoli.ArticoliAlimentari.articoliAlimentari;
+
+public class pane extends articoliAlimentari{
+    private int tempoCottura;
+    private int tempoLievitatura;
+    private boolean lievitatura;
+    private String descrizzione;
+
+
+    public void setLievitatura(boolean lievitatura) {
+        this.lievitatura = lievitatura;
+    }
+
+    public void setTempoCottura(int tempoCottura) {
+        this.tempoCottura = tempoCottura;
+    }
+
+    public void setTempoLievitatura(int tempoLievitatura) {
+        this.tempoLievitatura = tempoLievitatura;
+    }
+
+    public void setDescrizzione(String descrizzione) {
+        this.descrizzione = descrizzione;
+    }
+
+    public int getTempoCottura() {
+        return tempoCottura;
+    }
+
+    public int getTempoLievitatura() {
+        return tempoLievitatura;
+    }
+
+    public boolean getLievitatura(){
+        return lievitatura;
+    }
+
+    public String getDescrizzione() {
+        return descrizzione;
+    }
+
+
+    public boolean Cambia_Quantita_articolo(float quantita){
+
+        if (getQuantita_articolo() > quantita && getQuantita_articolo() > 0){
+            setQuantita_articolo(quantita);
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
 }
