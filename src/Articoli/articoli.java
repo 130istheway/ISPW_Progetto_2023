f9622c630bcf89462c2f6e56f22f013dc17e7d7b
Stefy <l.brigido45@gmail.com>
2023-10-28 21:42:46 +0200

    Update articoli.java in modo da essere una sottoclasse astratta

--- src/Articoli/articoli.java
+++ src/Articoli/articoli.java
@@ -1,5 +1,43 @@
 package Articoli;
 
-public class articoli {
+
+
+public abstract class articoli {
+    private int id;
+    private String nome_articolo;
+    private double prezzo_articolo;
+    protected float quantita_articolo;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome_articolo(String nome_articolo) {
+        this.nome_articolo = nome_articolo;
+    }
+
+    public void setPrezzo_articolo(double prezzo_articolo) {
+        this.prezzo_articolo = prezzo_articolo;
+    }
+
+    public void setQuantita_articolo(float quantita_articolo) {
+        this.quantita_articolo = quantita_articolo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome_articolo() {
+        return nome_articolo;
+    }
+    
+    public double getPrezzo_articolo() {
+        return prezzo_articolo;
+    }
+
+    public float getQuantita_articolo() {
+        return quantita_articolo;
+    }
     
 }
