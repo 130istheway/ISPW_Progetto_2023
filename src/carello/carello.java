40657f7df21d6e7339a7488d570034dc04d84f50
Stefy <l.brigido45@gmail.com>
2023-10-30 21:46:04 +0100

    Create carello.java

---
+++ src/carello/carello.java
@@ -0,0 +1,10 @@
+package carello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Articoli.articoli;
+
+public class carello {
+    List<articoli> carellino = new ArrayList<articoli>();
+}
