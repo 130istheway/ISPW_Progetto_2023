7cb9a400ddb35d1bf516c154dd5d9e95f15e9558
Stefy -0310135- <l.brigido45@gmail.com>
2023-10-31 01:25:59 +0100

    Update carello.java
    in teoria è quello che gli vorrei far fare

--- src/carello/carello.java
+++ src/carello/carello.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 import Articoli.articoli;
 
+/**
+ * Questra dpvrebbe essere la classe che gestisce il carello
+ * utente contiene i dati di utente in modo da poterlo reperire nel db
+ * carellino è una lista che contiene gli articoli, usando il polimorfismo un articolo può contenere tutti gli articoli derivati da lui
+ */
 public class carello {
+    private Utente utente;
     List<articoli> carellino = new ArrayList<articoli>();
 }
