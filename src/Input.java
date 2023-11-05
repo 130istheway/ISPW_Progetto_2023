b96c3713156dc87f7adddc3b53ce9a6fd37aa0d0
Stefy <l.brigido45@gmail.com>
2023-11-05 01:46:35 +0100

    creato un primo programma per testare il funzionamento del costruttore della classe pane
    creato il programma con gli input per richiamare il costruttore di pane, verificare che l'idea della lista che funge su più tipi funzioni realmente

---
+++ src/Input.java
@@ -0,0 +1,71 @@
+/**
+ * @author Stefano
+ * @author Simone
+ */
+
+import Articoli.ArticoliAlimentari.pane;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Input {
+    public static void main(String[] args) {
+        pane nuovo = new pane();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cosa si desidera inserire? pane|pizza");
+        String scelta = scanner.nextLine();
+
+        if (scelta.equals("pane")) {
+            
+            System.out.print("Inserisci il nome dell'articolo: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Inserisci il prezzo dell'articolo: ");
+            double prezzo = scanner.nextDouble();
+
+            System.out.print("Inserisci la quantita dell'articolo: ");
+            float quantita = scanner.nextFloat();
+
+
+            List<String> ingredienti = new ArrayList<String>();
+            boolean condition = false;
+
+            do  {
+                scanner.nextLine();
+                System.out.print("Inserire l'ingrediente della pizza: ");
+                String ingrediente = scanner.nextLine();
+
+                ingredienti.add(ingrediente);
+
+                System.out.print("Se ne vuole inserire un altro (true/false): ");
+                condition = scanner.nextBoolean();
+            } while (condition);
+
+
+            System.out.print("Inserisci il peso dell'articolo: ");
+            double peso = scanner.nextDouble();
+
+            System.out.println("Inserire il tempo di cottura del pane: ");
+            int tempoCottura = scanner.nextInt();
+
+            System.out.println("Inserire il tempo di lievitatura del pane: ");
+            int tempoLievitattura = scanner.nextInt();
+
+            System.out.print("Inserisci se la lievitatura è naturale o meno (true/false): ");
+            boolean lievitatura = scanner.nextBoolean();
+
+            scanner.nextLine();
+            System.out.print("Inserisci la descrizione dell'articolo: ");
+            String descri = scanner.nextLine();
+
+            nuovo = new pane(nome, prezzo, quantita, ingredienti, peso, tempoCottura, tempoLievitattura, lievitatura, descri);
+        }
+
+        System.out.println(nuovo.getId() + " " + nuovo.getLievitatura()+ " " + nuovo.getDescrizzione());
+
+        scanner.close();
+    }
+}
