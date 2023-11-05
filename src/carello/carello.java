27989d07c5d2313d8ff3f2936258cb8a4345b86e
Stefy <l.brigido45@gmail.com>
2023-11-05 01:46:46 +0100

    Update carello.java

--- src/carello/carello.java
+++ src/carello/carello.java
@@ -9,8 +9,32 @@ import Articoli.articoli;
  * Questra dpvrebbe essere la classe che gestisce il carello
  * utente contiene i dati di utente in modo da poterlo reperire nel db
  * carellino è una lista che contiene gli articoli, usando il polimorfismo un articolo può contenere tutti gli articoli derivati da lui
+ * dovrebbe esserci anche il metodo che cntrolla che il pagamento sia avvenuto, inserire un modo per saltare il controllo o fare in modo che inserendo una specifica carta si possa avere la transazione gia eseguita solo per test, per poi pensare a crteare un vero metodo per pagare "realmente"
  */
 public class carello {
-    private Utente utente;
+    //private Utente utente;
     List<articoli> carellino = new ArrayList<articoli>();
+    boolean pagato = false;
+
+    public carello(List<articoli> carello, boolean pagato) {
+        this.carellino = carello;
+        this.pagato = pagato;
+    }
+
+    public void setCarellino(List<articoli> carello) {
+        this.carellino = carello;
+    }
+
+    public void setPagato(boolean pagato) {
+        this.pagato = pagato;
+    }
+
+    public List<articoli> getCarellino() {
+        return carellino;
+    }
+
+    public boolean getPagato(){
+        return pagato;
+    }
+
 }
